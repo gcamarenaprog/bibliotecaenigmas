7df211a8f7afa5d8d2a2ae5a6e8f0a226f4d93d8
gcamarenaprog <gcamarenaprog@outlook.com>
2025-08-25 16:53:44 -0600

    - Refactor 404.php file.

--- 404.php
+++ 404.php
@@ -1,61 +1,58 @@
-<?php get_header(); ?>
-<?php global $is_IE ?>
-	<div class="content">
+<?php
+  /*
+    Template: 		Biblioteca Enigmas
+    Author: 			Guillermo Camarena
+    Description: 	This file shows 404 error web page
+    Date:         25-08-2025
+    */
+?>
 
-		<div class="post error404">
-			<div class="post-inner">
-				<div class="title-404"><?php _eti( '404 :(' ); ?></div>
-				<h2 class="post-title"><?php _eti( 'Not Found' ); ?></h2>
-				<div class="clear"></div>
-				<div class="entry">
-					<p><?php _eti( 'Apologies, but the page you requested could not be found. Perhaps searching will help.' ); ?></p>
+<?php get_header (); ?>
+<?php global $is_IE; ?>
 
-					<div class="search-block-large">
-						<form method="get" action="<?php echo home_url(); ?>/">
-							<button class="search-button" type="submit" value="<?php if( !$is_IE ) _eti( 'Search' , 'tie' ) ?>"><i class="fa fa-search"></i></button>
-							<input type="text" id="s" name="s" value="<?php _eti( 'Search' ) ?>" onfocus="if (this.value == '<?php _eti( 'Search' ) ?>') {this.value = '';}" onblur="if (this.value == '') {this.value = '<?php _eti( 'Search' ) ?>';}"  />
-						</form>
-					</div><!-- .search-block /-->
-				</div><!-- .entry /-->
+  <div class="content">
+    <div class="post error404">
+      <div class="post-inner">
 
-				<?php
-					$original_post = $post;
+        <!-- Image /-->
+        <div class="image-404">
+          <img src="<?php echo get_template_directory_uri () . '/library/images/images/404.png'; ?>" alt="">
+        </div>
 
-					$args = array( 'posts_per_page'=> 4 , 'no_found_rows' => 1 );
-					$related_query = new wp_query( $args );
-					if( $related_query->have_posts() ) : $count=0;
-				?>
+        <!-- 404 :( /-->
+        <div class="title-404">
+          <?php _eti ('404 :('); ?>
+        </div>
 
-				<section id="related_posts">
-					<div class="block-head">
-						<h3><?php _eti( 'Check Also' ); ?></h3><div class="stripe-line"></div>
-					</div>
-					<div class="post-listing">
-						<?php while ( $related_query->have_posts() ) : $related_query->the_post()?>
-						<div <?php tie_post_class('related-item'); ?>>
-							<?php if ( function_exists("has_post_thumbnail") && has_post_thumbnail() ) : ?>
-							<div class="post-thumbnail">
-								<a href="<?php the_permalink(); ?>" rel="bookmark">
-									<?php the_post_thumbnail( 'tie-medium' ); ?>
-									<span class="fa overlay-icon"></span>
-								</a>
-							</div><!-- post-thumbnail /-->
-							<?php endif; ?>
-							<h3><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
-							<p class="post-meta"><?php tie_get_time() ?></p>
-						</div>
-						<?php endwhile;?>
-						<div class="clear"></div>
-					</div>
-				</section>
-				<?php
-				endif;
+        <!-- Not found text /-->
+        <div class="not-found-404">
+          <p><?php _eti ('Not Found'); ?></p>
+        </div>
+        
+        <!-- Message text /-->
+        <p class="text-center mt10 ">
+          <?php _eti ('Apologies, but the page you requested could not be found. Perhaps searching will help.'); ?>
+        </p>
 
-				$post = $original_post;
-				wp_reset_query();
-				?>
+        <!-- Go to home /-->
+        <p class="text-center mt10 ">
+          <a style="text-decoration: none;" title='Ir al inicio' href="https://bibliotecaenigmas.com/">Ir al inicio</a>
+        </p>
 
-			</div><!-- .post-inner -->
-		</div><!-- .post-listing -->
-	</div>
-<?php get_footer(); ?>
+        <!-- Search / Start -->
+        <div class="search-block-large">
+          <form method="get" action="<?php echo home_url (); ?>/">
+            <button class="search-button" type="submit" value="<?php if (!$is_IE)
+              _eti ('Search', 'tie') ?>"><i class="fa fa-search"></i></button>
+            <input type="text" id="s" name="s" value="<?php _eti ('Search') ?>"
+                   onfocus="if (this.value == '<?php _eti ('Search') ?>') {this.value = '';}"
+                   onblur="if (this.value == '') {this.value = '<?php _eti ('Search') ?>';}"/>
+          </form>
+        </div>
+        <!-- Search / End -->
+
+      </div>
+    </div>
+  </div>
+
+<?php get_footer (); ?>
