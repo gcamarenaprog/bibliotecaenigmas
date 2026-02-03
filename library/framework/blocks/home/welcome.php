f646b0e0fb4d214c30aa629afe89cea497095804
gcamarenaprog <gcamarenaprog@outlook.com>
2026-02-03 03:36:18 -0600

    - Refactor the welcome.php file in the home directory.

--- library/framework/blocks/home/welcome.php
+++ library/framework/blocks/home/welcome.php
@@ -1,4 +1,5 @@
 <?php
+  
   /**
    * Template Name:      Biblioteca Enigmas
    * Theme URI:          https://github.com/gcamarenaprog/bibliotecaenigmas
@@ -8,33 +9,63 @@
    * Path:               /library/framework/blocks/home/
    * File name:          welcome.php
    * Description:        This file shows the welcome section in home page.
-   * Date:               25-08-2025
+   * Date:               03-02-2026
    */
 ?>
 
+<!--/Title-->
 <section>
   <div class="tb-head">
     <h1>= BIENVENIDO =</h1>
   </div>
 </section>
 
+<!--/Content-->
 <section>
   <div class="tb-box">
 
-    <!-- Intro /-->
+    <!--/Introducing-->
     <section>
-      <p class="m0 text-center"> <b>Biblioteca Enigmas / Información Sobre Fenómenos Forteanos</b><br>
-        Hecho en México el 9 de mayo de 2016, es un sitio de referencia con información sobre fenómenos forteanos.</p>
+      <h2 class="text-center" style="line-height: 1.20em;">BIBLIOTECA ENIGMAS<br>Información sobre fenómenos forteanos</h2>
+      <p class="text-center mt10">Hecho en México el <strong> 9 de mayo de 2016,</strong> es un sitio de referencia
+          sobre fenómenos forteanos. <br><a href="https://bibliotecaenigmas.com/acerca-de/" title="Leer más sobre el sitio."> Más sobre el sitio.</a></p>
     </section>
 
-    <hr class="brown">
-
-    <!-- Blockquote /-->
+    <!--/Blockquote-->
     <section>
-      <blockquote>
+
+      <blockquote class="mt10">
+        <hr>
         <p> "Vivimos una grandiosa novela, en un gran teatro, montado por gente inteligente que le gusta jugar a
-          las marionetas”. <br><b>— Guillermo Camarena.·. </b></p>
+          las marionetas”. <br><strong>— Guillermo Camarena.·. </strong></p>
+        <hr>
       </blockquote>
+
+    </section>
+
+    <!--/Donate button-->
+    <section>
+      <div class="text-center mt10 mb10">
+        <a href="https://bibliotecaenigmas.com/realizar-una-donacion/">
+          <img class="wp-image-22972 size-full"
+               title="¡Realizar una donación!"
+               src="https://bibliotecaenigmas.com/wp-content/themes/sahifa/library/images/images/donar.png"
+               alt="¡Realizar una donación!"
+               width="89"
+               height="26"/>
+        </a>
+      </div>
+    </section>
+
+    <!--/Advise-->
+    <section>
+      <div class="tb-advice">
+        <p>Si no está de acuerdo con las <a title="Ir a Políticas de Privacidad"
+                                            href="https://bibliotecaenigmas.com/politicas-de-privacidad/"> Políticas de
+            Privacidad</a> y <a title="Ir a Términos y Condiciones"
+                                href="https://bibliotecaenigmas.com/terminos-y-condiciones/">
+            Términos y Condiciones</a> deberá reservarse el derecho acceso al sitio.</p>
+      </div>
     </section>
 
   </div>
