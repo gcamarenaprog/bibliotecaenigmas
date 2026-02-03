71bc8235e1b98925a3a67ef01fc236008ea53a3f
gcamarenaprog <gcamarenaprog@outlook.com>
2026-02-03 03:55:03 -0600

    - Refactor the last-post-book.php file in the home directory.

--- library/framework/blocks/home/last-post-book.php
+++ library/framework/blocks/home/last-post-book.php
@@ -1,4 +1,5 @@
 <?php
+  
   /**
    * Template Name:      Biblioteca Enigmas
    * Theme URI:          https://github.com/gcamarenaprog/bibliotecaenigmas
@@ -8,25 +9,24 @@
    * Path:               /library/framework/blocks/home/
    * File name:          last-post-book.php
    * Description:        This file shows the last post book section in home page.
-   * Date:               25-08-2025
+   * Date:               03-02-2026
    */
 ?>
 
 <section>
   <div class="tb-head">
-    <h1>= LO ÚLTIMO EN BIBLIOTECA =</h1>
+    <h1>= ÚLTIMOS LIBROS =</h1>
   </div>
 </section>
+
 <section>
   <div class="tb-box">
-
-    <div class="container_">
-      <div class="row_">
-
-        <?php
+    <div class="row_">
+      
+      <?php
         global $block, $get_meta;
-        wp_enqueue_script('tie-cycle');
-
+        $paged = -1;
+        
         $arguments = array(
           'post_type' => 'book',
           'posts_per_page' => 3,
@@ -34,106 +34,106 @@
           'tax_query' => array(
             array(
               'taxonomy' => 'genre',
-              'field'    => 'term_id',
-              'terms'    => array(1523),
+              'field' => 'term_id',
+              'terms' => array(1523),
               'operator' => 'NOT IN',
             ),
           ),
         );
-
+        
         $wp_query = new WP_Query($arguments);
+        
+        if ($wp_query->have_posts ()) : while ($wp_query->have_posts ()) : $wp_query->the_post ();
+          
+          // Get data of the books
+          $fullTitle = get_the_title ();
+          $title = getTitle ($fullTitle);
+          ?>
 
-        if ($wp_query->have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post();
+          <div class="col_-xxl-4 col_-xl-4  col_-md-4 col_-sm-12 col_xs-12 text-center">
+            <article>
+              <div class="tb-card-book">
+                <?php if (function_exists ("has_post_thumbnail") && has_post_thumbnail ()) : ?>
 
-            // Get data of the books
-            $completeTitleOfBook = get_the_title();
-            $titleOfBook = getTitle($completeTitleOfBook);
-        ?>
-
-            <div class="col_-xxl-4 col_-xl-4  col_-md-4 col_-sm-12 col_xs-12">
-              <article class="card">
-
-                <!-- Cover book -->
-                <?php if (function_exists("has_post_thumbnail") && has_post_thumbnail()) : ?>
-
+                  <!-- Cover book -->
                   <section>
-                    <div class="tbh-thumbnail-b">
-
-                      <?php
-                      $be_theme_check = get_post_meta($post->ID, 'be_theme_check', true);
+                    <?php
+                      $be_theme_check = get_post_meta ($post->ID, 'be_theme_check', true);
                       if ($be_theme_check != 'yes') {
-                        echo '<div  class="post-thumbnail tie_check tie-appear tbh-post-thumbnail-b"';
+                        echo '<div  class="post-thumbnail tie_check tie-appear tb-card-book-thumbnail"';
                       } else {
-                        echo '<div  class="post-thumbnail tie_book tie-appear tbh-post-thumbnail-b">';
+                        echo '<div  class="post-thumbnail tie_book tie-appear tb-card-book-thumbnail">';
                       }
-                      ?>
-                      <a href="<?php the_permalink(); ?>" rel="bookmark">
-                        <?php the_post_thumbnail(); ?>
-                        <li
-                          class="fa overlay-icon tbh-overlay-icon-b">
-                        </li>
-                      </a>
-
-                    </div>
+                    ?>
+                    <a href="<?php the_permalink (); ?>" rel="bookmark">
+                      <?php the_post_thumbnail (); ?>
+                      <li
+                          class="fa overlay-icon tb-card-book-overlay-icon">
+                      </li>
+                    </a>
                   </section>
-
+                
                 <?php endif; ?>
 
                 <!-- Data -->
                 <section>
-                  <div class="book">
+                  <div class="tb-card-book-data">
 
                     <!-- Title -->
-                    <h1>
-                      <a class="title" href="<?php the_permalink(); ?>" rel="bookmark">
-                        <?php echo $titleOfBook; ?> <span class="paragraph-end"></span>
+                    <div class="tb-card-book-title">
+                      <a href="<?php the_permalink (); ?>" rel="bookmark">
+                        <?php echo $title; ?> <span class="tb-card-book-title-paragraph-end"></span>
                       </a>
-                    </h1>
+                    </div>
 
                     <!-- Writer -->
-                    <div class="writer">
-                      <?php echo get_the_term_list($post->ID, 'writer', '', ', ', ''); ?><span
-                        class="paragraph-end-writer"></span>
+                    <div class="tb-card-book-writer">
+                      <?php $writer = get_the_term_list ($post->ID, 'writer', '', ', ', '');
+                        echo $writer;
+                      ?>
+                      <span class="tb-card-book-writer-paragraph-end "></span>
                     </div>
 
                     <!-- Excerpt -->
-                    <div class="excerpt">
-                      <p> <?php the_content(); ?></p>
-                      <span class="paragraph-end"></span>
+                    <div class="tb-card-book-excerpt">
+                      <p> <?php the_excerpt (); ?></p>
+                      <span class="tb-card-book-excerpt-paragraph-end"></span>
                     </div>
 
                   </div>
                 </section>
 
-              </article><!--/.card-->
-
-            </div><!--/.cols-->
-
-          <?php endwhile; ?>
+              </div>
+            </article><!--/.card-->
+          </div><!--/.cols-->
+        
+        <?php endwhile; ?>
 
           <!-- No books -->
         <?php else : ?>
 
-          <!-- No books message /-->
-          <article>
-            <div class="item-list">
-              <div class="tb-no-more-books">
-                No hay más libros.
-              </div>
+          <!-- No more posts /-->
+          <div class="col_-12">
+            <div class="row_">
+              <p class="text-center">No hay publicaciones.</p>
             </div>
-          </article>
-
+          </div>
+        
         <?php endif ?>
 
+      <div class="col_-12">
+
+        <!-- Button -->
         <section>
-          <div class="tbh-button">
-            <a title="Ir a Biblioteca" href="https://bibliotecaenigmas.com/books/"
-              class="shortc-button tbh  ">Ir a Biblioteca</a>
+          <div class="text-center">
+            <a title="Ir a la sección de Libros"
+               href="https://bibliotecaenigmas.com/books/"
+               class="tb-button">Ir a la sección</a>
           </div>
         </section>
 
-      </div>
-    </div>
 
+      </div><!--/.col_-12-->
+    </div><!--/.row_-->
   </div><!--/.tb-box-->
 </section>
