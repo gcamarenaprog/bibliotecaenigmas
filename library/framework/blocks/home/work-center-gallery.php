cf3a6926e114aa3b398407f6959b388d1ddb2cae
gcamarenaprog <gcamarenaprog@outlook.com>
2026-02-03 03:51:15 -0600

    - Refactor the work-center-gallery.php file in the home directory.

---
+++ library/framework/blocks/home/work-center-gallery.php
@@ -0,0 +1,89 @@
+<?php
+  
+  /**
+   * Template Name:      Biblioteca Enigmas
+   * Theme URI:          https://github.com/gcamarenaprog/bibliotecaenigmas
+   * Description Theme:  Sahifa theme personalized for bibliotecaenigmas.com website!
+   * Author:             Guillermo Camarena
+   * Author URL:         http://gcamarenaprog.com
+   * Path:               /library/framework/blocks/gallery/
+   * File name:          work-center-gallery.php
+   * Description:        This file shows the work center gallery section in gallery page.
+   * Date:               03-02-2026
+   */
+?>
+
+<!--/Title-->
+<section>
+  <div class="tb-head">
+    <h1>= GALERÍA DEL ESPACIO DE TRABAJO =</h1>
+  </div>
+</section>
+
+<!--/Content-->
+<section>
+  <div class="tb-box">
+
+    <!--/Photos-->
+    <div class="row_">
+
+      <div class="row_ mt20 mb20">
+
+        <!--/Photo 1-->
+        <div class="col_-xxl-4 col_-xl-4 col_-md-4 col_-sm-6 col_xs-12 text-center">
+          <figure class="wp-caption aligncenter" style="transform: rotate(5deg); width: 300px;">
+            <a href="https://bibliotecaenigmas.com/wp-content/themes/sahifa/library/images/gallery/img1.jpg">
+              <img class="wp-image size-full"
+                   title="Espacio de digitalización de audio y video."
+                   src="https://bibliotecaenigmas.com/wp-content/themes/sahifa/library/images/gallery/img1.jpg"
+                   alt="Espacio de digitalización de audio y video."/>
+            </a>
+            <div class="wp-caption-text">Espacio de digitalización de audio y video.</div>
+          </figure>
+        </div>
+
+        <!--/Photo 2-->
+        <div class="col_-xxl-4 col_-xl-4 col_-md-4 col_-sm-6 col_xs-12 text-center">
+          <figure class="wp-caption aligncenter"
+                  style="transform: rotate(-5deg);
+                  width: 300px;">
+            <a href="https://bibliotecaenigmas.com/wp-content/themes/sahifa/library/images/gallery/img2.jpg">
+              <img class="wp-image size-full"
+                   title="Espacio de maquetación, edición y publicación."
+                   src="https://bibliotecaenigmas.com/wp-content/themes/sahifa/library/images/gallery/img2.jpg"
+                   alt="Espacio de maquetación, edición y publicación."/>
+            </a>
+            <div class="wp-caption-text">Espacio de maquetación, edición y publicación.</div>
+          </figure>
+        </div>
+
+        <!--/Photo 3-->
+        <div class="col_-xxl-4 col_-xl-4 col_-md-4 col_-sm-12 col_xs-12 text-center">
+          <figure class="wp-caption aligncenter"
+                  style="transform: rotate(5deg); width: 300px;">
+            <a href="https://bibliotecaenigmas.com/wp-content/uploads/2025/11/img3.jpg">
+              <img class="wp-image size-full"
+                   title="Espacio de digitalización de libros."
+                   src="https://bibliotecaenigmas.com/wp-content/uploads/2025/11/img3.jpg"
+                   alt="Espacio de digitalización de libros."/>
+            </a>
+            <div class="wp-caption-text">Espacio de digitalización de libros.</div>
+          </figure>
+        </div>
+
+      </div>
+      &nbsp;
+
+    </div><!--/.row_-->
+
+    <!--/Button-->
+    <section>
+      <div class="text-center">
+        <a title="Ir a Galería del Espacio de Trabajo"
+           href="https://bibliotecaenigmas.com/espacio-de-trabajo/" class="tb-button">
+          Ir a la sección</a>
+      </div>
+    </section>
+
+  </div><!--/.tb-box-->
+</section>
