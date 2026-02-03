8ab950710c0f00178aa0c956139cb862fe79880f
gcamarenaprog <gcamarenaprog@outlook.com>
2026-02-03 08:18:44 -0600

    - Refactor last-books.php file.

--- library/framework/blocks/taxonomy/last-books.php
+++ library/framework/blocks/taxonomy/last-books.php
@@ -32,15 +32,15 @@ if ($slug == 'coleccion-de-libros') {
 ?>
 
 <?php if ($parent_term_id): // Books collection or Encyclopedias collection
-?>
+  ?>
 
   <?php
   $taxonomies = array(
-    'genre',
+      'genre',
   );
   $args = array(
-    'parent' => $parent_term_id,
-    'orderby' => 'term_order',
+      'parent' => $parent_term_id,
+      'orderby' => 'term_order',
   );
   $terms = get_terms($taxonomies, $args);
   ?>
@@ -56,7 +56,7 @@ if ($slug == 'coleccion-de-libros') {
     </div>
   </section>
 
-  <!-- Content / Start -->
+  <!-- Content -->
   <section>
     <div class="tb-box">
       <div class="row_">
@@ -68,17 +68,17 @@ if ($slug == 'coleccion-de-libros') {
           $name = $cat->name;
           $home = get_home_url();
           $link = $home . '/genre/' . $term;
-        ?>
+          ?>
 
           <div class="col_-xxl-4 col_-xl-4 col_-md-4  col_-sm-6 col_xs-12 text-center mb10">
             <div class="tb-card-blog">
               <div class="post-thumbnail tie_book tb-card-blog-thumbnail tie-appear">
                 <a href="<?php echo $link; ?>" rel="bookmark">
                   <img width="660" height="380"
-                    src=" <?php echo $imageUrl; ?> "
-                    class="attachment-post-thumbnail size-post-thumbnail wp-post-image tie-appear"
-                    alt="" decoding="async"
-                    fetchpriority="high">
+                       src=" <?php echo $imageUrl; ?> "
+                       class="attachment-post-thumbnail size-post-thumbnail wp-post-image tie-appear"
+                       alt="" decoding="async"
+                       fetchpriority="high">
                   <li class="fa fa-book overlay-icon tb-card-blog-overlay-icon">
                   </li>
                 </a>
@@ -91,30 +91,28 @@ if ($slug == 'coleccion-de-libros') {
             </div>
           </div>
 
-        <?php
+          <?php
         }
         ?>
       </div><!--/.row_-->
     </div><!--/.tb-box-->
   </section>
 
-<?php else: // Books and multimedia genres
-?>
+<?php else: // Books and multimedia genres  ?>
 
-  <?php  $genre = get_the_taxonomies();  ?>
 
   <!-- Title /-->
   <section>
     <div class="tb-head">
-      <?php if (str_contains($genre['genre'], 'Multimedia')):
+      <?php if ($slug == 'multimedia'):
         $tieIcon = 'tie_play';
 
-      ?>
+        ?>
         <h1>= ÚLTIMAS PUBLICACIONES EN <?php echo $term->name; ?> =</h1>
 
       <?php else:
         $tieIcon = 'tie_book';
-      ?>
+        ?>
         <h1>= ÚLTIMAS LIBROS DE <?php echo $term->name; ?> =</h1>
       <?php endif; ?>
     </div>
@@ -128,72 +126,72 @@ if ($slug == 'coleccion-de-libros') {
         <?php
         if ($wp_query->have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post();
 
-            // Get data of the books
-            $completeTitleOfBook = get_the_title();
-            $title = getTitle($completeTitleOfBook);
-        ?>
+          // Get data of the books
+          $completeTitleOfBook = get_the_title();
+          $title = getTitle($completeTitleOfBook);
+          ?>
 
-            <div class="col_-xxl-4 col_-xl-4  col_-md-4 col_-sm-6 col_xs-12 text-center">
-              <article>
-                <div class="tb-card-book">
-                  <?php if (function_exists("has_post_thumbnail") && has_post_thumbnail()) : ?>
+          <div class="col_-xxl-4 col_-xl-4  col_-md-4 col_-sm-6 col_xs-12 text-center">
+            <article>
+              <div class="tb-card-book">
+                <?php if (function_exists("has_post_thumbnail") && has_post_thumbnail()) : ?>
 
-                    <!-- Cover book -->
-                    <section>
-                      <?php
-                      $be_theme_check = get_post_meta($post->ID, 'be_theme_check', true);
-                      if ($be_theme_check != 'yes') {
-                        echo '<div  class="post-thumbnail tie_check tie-appear tb-card-book-thumbnail">';
-                      } else {
-                        echo '<div  class="post-thumbnail ';
-                        echo $tieIcon;
-                        echo ' tie-appear tb-card-book-thumbnail">';
-                      }
-                      ?>
-                      <a href="<?php the_permalink(); ?>" rel="bookmark">
-                        <?php the_post_thumbnail(); ?>
-                        <li
-                          class="fa overlay-icon tb-card-book-overlay-icon">
-                        </li>
-                      </a>
-                    </section>
-
-                  <?php endif; ?>
-
-                  <!-- Data -->
+                  <!-- Cover book -->
                   <section>
-                    <div class="tb-card-book-data">
-
-                      <!-- Title -->
-                      <div class="tb-card-book-title">
-                        <a href="<?php the_permalink(); ?>" rel="bookmark">
-                          <?php echo $title; ?> <span
-                            class="tb-card-book-title-paragraph-end"></span>
-                        </a>
-                      </div>
-
-                      <!-- Writer -->
-                      <div class="tb-card-book-writer">
-                        <?php $writer = get_the_term_list($post->ID, 'writer', '', ', ', '');
-                        echo $writer;
-                        ?>
-                        <span class="tb-card-book-writer-paragraph-end "></span>
-                      </div>
-
-                      <!-- Excerpt -->
-                      <div class="tb-card-book-excerpt">
-                        <p> <?php the_excerpt(); ?></p>
-                        <span class="tb-card-book-excerpt-paragraph-end"></span>
-                      </div>
-
-                    </div>
+                    <?php
+                    $be_theme_check = get_post_meta($post->ID, 'be_theme_check', true);
+                    if ($be_theme_check != 'yes') {
+                      echo '<div  class="post-thumbnail tie_check tie-appear tb-card-book-thumbnail">';
+                    } else {
+                      echo '<div  class="post-thumbnail ';
+                      echo $tieIcon;
+                      echo ' tie-appear tb-card-book-thumbnail">';
+                    }
+                    ?>
+                    <a href="<?php the_permalink(); ?>" rel="bookmark">
+                      <?php the_post_thumbnail(); ?>
+                      <li
+                          class="fa overlay-icon tb-card-book-overlay-icon">
+                      </li>
+                    </a>
                   </section>
 
-                </div>
-              </article><!--/.card-->
-            </div><!--/.cols-->
+                <?php endif; ?>
 
-          <?php endwhile; ?>
+                <!-- Data -->
+                <section>
+                  <div class="tb-card-book-data">
+
+                    <!-- Title -->
+                    <div class="tb-card-book-title">
+                      <a href="<?php the_permalink(); ?>" rel="bookmark">
+                        <?php echo $title; ?> <span
+                            class="tb-card-book-title-paragraph-end"></span>
+                      </a>
+                    </div>
+
+                    <!-- Writer -->
+                    <div class="tb-card-book-writer">
+                      <?php $writer = get_the_term_list($post->ID, 'writer', '', ', ', '');
+                      echo $writer;
+                      ?>
+                      <span class="tb-card-book-writer-paragraph-end "></span>
+                    </div>
+
+                    <!-- Excerpt -->
+                    <div class="tb-card-book-excerpt">
+                      <p> <?php the_excerpt(); ?></p>
+                      <span class="tb-card-book-excerpt-paragraph-end"></span>
+                    </div>
+
+                  </div>
+                </section>
+
+              </div>
+            </article><!--/.card-->
+          </div><!--/.cols-->
+
+        <?php endwhile; ?>
 
           <!-- No books -->
         <?php else : ?>
@@ -210,9 +208,10 @@ if ($slug == 'coleccion-de-libros') {
       </div><!--/.row_-->
     </div><!--/.tb-box-->
 
+    <!-- Pagination /-->
     <section>
       <div class="row_">
-        <!-- Pagination /-->
+
         <?php if ($wp_query->max_num_pages > 1)
           tie_pagenavi(); ?>
       </div>
