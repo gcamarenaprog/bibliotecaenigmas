cb9fecaecba3742362af1fbb156438822580c636
gcamarenaprog <gcamarenaprog@outlook.com>
2026-02-23 15:56:46 -0600

    - TinyMCE BE Tools - Added images option.

--- library/js/tools.js
+++ library/js/tools.js
@@ -11,8 +11,8 @@
 
       menu: [
         {
-          text: 'Elementos',
-          icon: ' be-elements',
+          text: 'Encabezados',
+          icon: ' be-ico_imgs',
           menu: [
             {
               text: 'A la izquierda',
@@ -171,138 +171,176 @@
               }
             },
           ]
-
         },
         {
-          text: 'Línea punteada',
-          icon: ' be-shortcodes-icon',
+          text: 'Elementos',
+          icon: ' be-ico_imgs',
+          menu: [
+            {
+              text: 'Insertar línea',
+              icon: ' be-ico_summary',
 
-          onclick: function () {
-            editor.windowManager.open({
-              title: 'Una imágen a la izquierda',
-              tooltip: 'Añadir una imágen rotada a la izquierda',
-              body: [
-                {
-                  type: 'listbox',
-                  name: 'widthLine',
-                  id: 'widthLine',
-                  label: 'Ancho',
-
-                  'values': [
-                    {text: '1px', value: '1px'},
-                    {text: '2px', value: '2px'},
-                    {text: '3px', value: '3px'},
-                    {text: '4px', value: '4px'},
-                    {text: '5px', value: '5px'},
-                    {text: '6px', value: '6px'},
-                    {text: '7px', value: '7px'},
-                    {text: '8px', value: '8px'},
-                    {text: '9px', value: '9px'},
-                    {text: '10px', value: '10px'},
-                  ],
-                  onselect: function (evt, e) {
-                    console.log('clicked');
-                    //console.log(this.value);
-                    console.log(evt.target.settings.value);
-                    //  console.log(evt);
-                    // evt.data.exampleLine.textContent = "Nuevo Texto!";
-
+              onclick: function () {
+                var win;
+                var charMapPanel = {
+                  type: 'container',
+                  html: '<div><h1>dsfsdlfksd</h1></div>',
+                  onclick: function (e) {
+                    var target = e.target;
+                    if (/^(TD|DIV)$/.test(target.nodeName)) {
+                      var charDiv = getParentTd(target).firstChild;
+                      if (charDiv && charDiv.hasAttribute('data-chr')) {
+                        var charCodeString = charDiv.getAttribute('data-chr');
+                        var charCode = parseInt(charCodeString, 10);
+                        if (!isNaN(charCode)) {
+                          Actions.insertChar(editor, String.fromCharCode(charCode));
+                        }
+                        if (!e.ctrlKey) {
+                          win.close();
+                        }
+                      }
+                    }
                   },
+                  onmouseover: function (e) {
+                    console.log('test');
+                  }
+                };
 
-                },
+                editor.windowManager.open({
 
-                {
-                  type: 'listbox',
-                  name: 'boxAlignment',
-                  label: tieLang.shortcode_alignment,
-                  'values': [
-                    {text: '', value: ''},
-                    {text: tieLang.shortcode_right, value: 'alignright'},
-                    {text: tieLang.shortcode_left, value: 'alignleft'},
-                    {text: tieLang.shortcode_center, value: 'aligncenter'}
-                  ]
-                },
-                {
-                  type: 'textbox',
-                  name: 'CustomClass',
-                  label: tieLang.shortcode_class,
-                  value: ''
-                },
-                {
-                  type: 'textbox',
-                  name: 'BoxWidth',
-                  label: tieLang.shortcode_width,
-                  value: ''
-                },
-                {
-                  type: 'textbox',
-                  name: 'BoxContent',
-                  id: 'urlImg1',
-                  label: 'URL de la imágen',
-                  placeholder: 'https://bibliotecaenigmas.com/wp-content/themes/sahifa/library/js/no-image.jpg',
-                  minWidth: 300,
-                  value: 'test'
-                },
-                {
-                  type: 'label',
-                  name: 'exampleLine',
-                  id: 'exampleLine',
-                  minWidth: 300,
-                  text: 'test'
-                },
-                {
-                  type: 'label', // component type
-                  html: '<div><hr>fdsfds</div>'
-                }
+                  title: 'Special character',
+                  spacing: 10,
+                  padding: 10,
+                  items: [
+                    charMapPanel,
+                    {
+                      type: 'container',
+                      layout: 'flex',
+                      direction: 'column',
+                      align: 'center',
+                      spacing: 5,
+                      minWidth: 160,
+                      minHeight: 160,
+                      items: [
+                        {
+                          type: 'label',
+                          name: 'preview',
+                          text: ' ',
+                          style: 'font-size: 40px; text-align: center',
+                          border: 1,
+                          minWidth: 140,
+                          minHeight: 80
+                        },
+                        {
+                          type: 'spacer',
+                          minHeight: 20
+                        },
+                        {
+                          type: 'label',
+                          name: 'previewTitle',
+                          text: ' ',
+                          style: 'white-space: pre-wrap;',
+                          border: 1,
+                          minWidth: 140
+                        }
+                      ]
+                    }
+                  ],
+                  buttons: [{
+                    text: 'Close',
+                    onclick: function () {
+                      win.close();
+                    }
+                  }]
+                });
+              }
+            },
+          ]
+        },
+        {
+          text: 'Secciones',
+          icon: ' be-ico_imgs',
+          menu: [
+            {
+              text: 'Insertar línea',
+              icon: ' be-ico_summary',
 
+              onclick: function () {
+                var win;
+                var charMapPanel = {
+                  type: 'container',
+                  html: '<div><h1>dsfsdlfksd</h1></div>',
+                  onclick: function (e) {
+                    var target = e.target;
+                    if (/^(TD|DIV)$/.test(target.nodeName)) {
+                      var charDiv = getParentTd(target).firstChild;
+                      if (charDiv && charDiv.hasAttribute('data-chr')) {
+                        var charCodeString = charDiv.getAttribute('data-chr');
+                        var charCode = parseInt(charCodeString, 10);
+                        if (!isNaN(charCode)) {
+                          Actions.insertChar(editor, String.fromCharCode(charCode));
+                        }
+                        if (!e.ctrlKey) {
+                          win.close();
+                        }
+                      }
+                    }
+                  },
+                  onmouseover: function (e) {
+                    console.log('test');
+                  }
+                };
 
-              ],
-              onselect: function (e) {
-                console.log('clicked2');
-                console.log(e.target.settings.value);
-                let urlimg1 = document.querySelector('#exampleLine');
-                console.log(urlimg1);
-                urlimg1.innerHTML = '<p><hr class="brown" style="\n' +
-                  '    border-bottom: 5px dotted #2d2d2d !important;\n' +
-                  '    border-style: double !important;\n' +
-                  '    overflow: hidden !important;\n' +
-                  '    clear: both !important;\n' +
-                  '    margin-bottom: 10px !important;\n' +
-                  '    margin-top: 10px !important;\n' +
-                  '">';
-                // urlimg1.textContent  = 'sdf';
-                // console.log(evt);
+                editor.windowManager.open({
 
-                //  evt.data.exampleLine.textContent = "Nuevo Texto!";
-              },
-
-              onsubmit: function (e) {
-                editor.insertContent('<div class="row_ mt20 mb20">' +
-                  '<div class="col_-xxl-12 col_-xl-12 col_-md-12 col_-sm-12 col_xs-12 text-center">' +
-                  '<figure class="wp-caption aligncenter" style="transform: rotate(-5deg); width: 300px;">' +
-                  '<a href="' + e.data.BoxContent + '">' +
-                  '<img class="wp-image size-full" title="Text" src="' + e.data.BoxContent + '" alt="Text" />' +
-                  '</a>' +
-                  '<div class="wp-caption-text">Text</div>' +
-                  '</figure>' +
-                  '</div>' +
-                  '</div>');
-              },
-
-              /*          var el = document.getElementById("widthLine");
-                        var val = el[el.selectedIndex].value;
-                        alert(val);
-
-                        function cambiarTexto() {
-                        // Selecciona el elemento y cambia su texto
-                        document.getElementById('miEtiqueta').textContent = "Nuevo Texto!";*/
-              // }
-
-
-            });
-          }
-
-
+                  title: 'Special character',
+                  spacing: 10,
+                  padding: 10,
+                  items: [
+                    charMapPanel,
+                    {
+                      type: 'container',
+                      layout: 'flex',
+                      direction: 'column',
+                      align: 'center',
+                      spacing: 5,
+                      minWidth: 160,
+                      minHeight: 160,
+                      items: [
+                        {
+                          type: 'label',
+                          name: 'preview',
+                          text: ' ',
+                          style: 'font-size: 40px; text-align: center',
+                          border: 1,
+                          minWidth: 140,
+                          minHeight: 80
+                        },
+                        {
+                          type: 'spacer',
+                          minHeight: 20
+                        },
+                        {
+                          type: 'label',
+                          name: 'previewTitle',
+                          text: ' ',
+                          style: 'white-space: pre-wrap;',
+                          border: 1,
+                          minWidth: 140
+                        }
+                      ]
+                    }
+                  ],
+                  buttons: [{
+                    text: 'Close',
+                    onclick: function () {
+                      win.close();
+                    }
+                  }]
+                });
+              }
+            },
+          ]
         },
         {
           text: 'Columnas',
@@ -1040,159 +1078,8 @@
                 });
               }
             },
-
-
-            {
-              text: 'Insertar linea',
-              icon: ' be-ico_summary',
-              onclick: function () {
-                editor.windowManager.open({
-                  title: 'Añadir 2 imágenes',
-                  body: [
-
-                    {
-                      type: 'textbox',
-                      name: 'textBoxText',
-                      id: 'textBoxText',
-                      label: 'Texto',
-                      value: 'Texto',
-                      placeholder: 'Texto'
-                    },
-                    {
-                      type: 'textbox',
-                      name: 'textBoxURL',
-                      id: 'textBoxURL',
-                      label: 'URL de la imágen',
-                      placeholder: 'https://bibliotecaenigmas.com/wp-content/themes/sahifa/library/images/images/no-image.jpg',
-                      minWidth: 300,
-                      value: 'https://bibliotecaenigmas.com/wp-content/themes/sahifa/library/images/images/no-image.jpg'
-                    },
-                    {
-                      type: 'button',
-                      name: 'button',
-                      text: 'Seleccionar imágen',
-                      id: 'myButton',
-                      value: null,
-
-                      onclick: function () {
-                        let mediaUploader;
-                        if (mediaUploader) {
-                          mediaUploader.open();
-                          return;
-                        }
-                        mediaUploader = wp.media.frames.fle_frame = wp.media({
-                          title: 'Añadir imágen a la derecha',
-                          button: {
-                            text: 'Añadir'
-                          },
-                          multiple: false
-                        })
-                        mediaUploader.on('select', function () {
-                          attachment = mediaUploader.state().get('selection').first().toJSON();
-                          let textBoxURL = document.querySelector('#textBoxURL');
-                          textBoxURL.value = attachment['url'];
-                        });
-                        mediaUploader.open();
-                      }
-                    },
-                  ],
-                  onsubmit: function (e) {
-                    editor.insertContent('<div class="row_ mt20 mb20">' +
-                      '<div class="col_-xxl-12 col_-xl-12 col_-md-12 col_-sm-12 col_xs-12 text-center">' +
-                      '<figure class="wp-caption aligncenter" style="transform: rotate(5deg); width: ' + e.data.textBoxWidth + 'px;">' +
-                      '<a href="' + e.data.textBoxURL + '">' +
-                      '<img class="wp-image size-full" title="' + e.data.textBoxText + '" alt="' + e.data.textBoxText + '" src="' + e.data.textBoxURL + '" alt="' + e.data.textBoxText + '" />' +
-                      '</a>' +
-                      '<div class="wp-caption-text">' + e.data.textBoxText + '</div>' +
-                      '</figure>' +
-                      '</div>' +
-                      '</div>');
-                  },
-                });
-              }
-            },
-            {
-              text: 'Insertar línea',
-              icon: ' be-ico_summary',
-
-              onclick: function () {
-                var win;
-                var charMapPanel = {
-                  type: 'container',
-                  html: '<div><h1>dsfsdlfksd</h1></div>',
-                  onclick: function (e) {
-                    var target = e.target;
-                    if (/^(TD|DIV)$/.test(target.nodeName)) {
-                      var charDiv = getParentTd(target).firstChild;
-                      if (charDiv && charDiv.hasAttribute('data-chr')) {
-                        var charCodeString = charDiv.getAttribute('data-chr');
-                        var charCode = parseInt(charCodeString, 10);
-                        if (!isNaN(charCode)) {
-                          Actions.insertChar(editor, String.fromCharCode(charCode));
-                        }
-                        if (!e.ctrlKey) {
-                          win.close();
-                        }
-                      }
-                    }
-                  },
-                  onmouseover: function (e) {
-                    console.log('test');
-                  }
-                };
-
-                editor.windowManager.open({
-
-                  title: 'Special character',
-                  spacing: 10,
-                  padding: 10,
-                  items: [
-                    charMapPanel,
-                    {
-                      type: 'container',
-                      layout: 'flex',
-                      direction: 'column',
-                      align: 'center',
-                      spacing: 5,
-                      minWidth: 160,
-                      minHeight: 160,
-                      items: [
-                        {
-                          type: 'label',
-                          name: 'preview',
-                          text: ' ',
-                          style: 'font-size: 40px; text-align: center',
-                          border: 1,
-                          minWidth: 140,
-                          minHeight: 80
-                        },
-                        {
-                          type: 'spacer',
-                          minHeight: 20
-                        },
-                        {
-                          type: 'label',
-                          name: 'previewTitle',
-                          text: ' ',
-                          style: 'white-space: pre-wrap;',
-                          border: 1,
-                          minWidth: 140
-                        }
-                      ]
-                    }
-                  ],
-                  buttons: [{
-                    text: 'Close',
-                    onclick: function () {
-                      win.close();
-                    }
-                  }]
-                });
-              }
-            },
           ]
         },
-
         {
           text: 'YouTube',
           icon: ' be-ico_youtube',
