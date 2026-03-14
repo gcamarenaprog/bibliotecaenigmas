a6fa3b54758e3364daf7a49b41996e359cf98c9d
gcamarenaprog <gcamarenaprog@outlook.com>
2026-03-14 04:46:15 -0600

    - Fixed number of books on genre, editorial and writer taxonomy.

--- library/framework/blocks/book/search/taxonomy-search.php
+++ library/framework/blocks/book/search/taxonomy-search.php
@@ -1,20 +1,26 @@
 <?php
-  /**
-   * Template Name:      Biblioteca Enigmas
-   * Theme URI:          https://github.com/gcamarenaprog/bibliotecaenigmas
-   * Description Theme:  Sahifa theme personalized for bibliotecaenigmas.com website!
-   * Author:             Guillermo Camarena
-   * Author URL:         http://gcamarenaprog.com
-   * Path:               /library/framework/blocks/book/search/
-   * File name:          taxonomy-search.php
-   * Description:        This file shows taxonomy search page.
-   * Date:               25-08-2025
-   */
+/**
+ * Template Name:      Biblioteca Enigmas
+ * Theme URI:          https://github.com/gcamarenaprog/bibliotecaenigmas
+ * Description Theme:  Sahifa theme personalized for bibliotecaenigmas.com website!
+ * Author:             Guillermo Camarena
+ * Author URL:         http://gcamarenaprog.com
+ * Path:               /library/framework/blocks/book/search/
+ * File name:          taxonomy-search.php
+ * Description:        This file shows taxonomy search page.
+ * Date:               14-03-2026
+ */
 ?>
 
 <?php
-  
-  echo do_shortcode ('[wp-datatable id="table" fat="LEVEL"]
+$taxonomyName = '';
+$taxonomy = get_query_var('taxonomyName');
+$taxonomyTerms = get_terms($taxonomy);
+
+if ($taxonomy == 'genre') {
+    $taxonomyName = 'GÉNERO';
+
+    echo do_shortcode('[wp-datatable id="table" fat="LEVEL"]
   paging: true,
   responsive: true,
   search: true,
@@ -22,9 +28,9 @@
   columnDefs: [{ "orderable": true, "targets": [0, 1] }],
   language: {
     "search": "Buscar",
-    "lengthMenu": "Mostrar _MENU_ libros",
-    "info":           "Mostrando _START_ a _END_ de _TOTAL_ libros",
-    "infoEmpty":      "Mostrando 0 a 0 de 0 libros",
+    "lengthMenu": "Mostrar _MENU_ géneros",
+    "info":           "Mostrando _START_ a _END_ de _TOTAL_ géneros",
+    "infoEmpty":      "Mostrando 0 a 0 de 0 géneros",
     "paginate": {
       "first":      "Primera",
       "last":       "Última",
@@ -33,56 +39,98 @@
     },
   },
   [/wp-datatable]');
-  
-  $taxonomy = get_query_var ('taxonomyName');
-  $taxonomyTerms = get_terms ($taxonomy);
-  if ($taxonomy == 'genre') {
-    $taxonomyName = 'GÉNERO';
-  }
-  if ($taxonomy == 'writer') {
+}
+
+if ($taxonomy == 'writer') {
     $taxonomyName = 'AUTOR';
-  }
-  if ($taxonomy == 'editorial') {
+
+    echo do_shortcode('[wp-datatable id="table" fat="LEVEL"]
+  paging: true,
+  responsive: true,
+  search: true,
+  lengthMenu: [ [5, 10, 25, 50, 100, -1], [5, 10, 25, 50, 100, "Todos"] ],
+  columnDefs: [{ "orderable": true, "targets": [0, 1] }],
+  language: {
+    "search": "Buscar",
+    "lengthMenu": "Mostrar _MENU_ autores",
+    "info":           "Mostrando _START_ a _END_ de _TOTAL_ autores",
+    "infoEmpty":      "Mostrando 0 a 0 de 0 autores",
+    "paginate": {
+      "first":      "Primera",
+      "last":       "Última",
+      "next":       "»",
+      "previous":   "«"
+    },
+  },
+  [/wp-datatable]');
+}
+
+if ($taxonomy == 'editorial') {
     $taxonomyName = 'EDITORIAL';
-  }
-  $index = 0;
+
+    echo do_shortcode('[wp-datatable id="table" fat="LEVEL"]
+  paging: true,
+  responsive: true,
+  search: true,
+  lengthMenu: [ [5, 10, 25, 50, 100, -1], [5, 10, 25, 50, 100, "Todos"] ],
+  columnDefs: [{ "orderable": true, "targets": [0, 1] }],
+  language: {
+    "search": "Buscar",
+    "lengthMenu": "Mostrar _MENU_ editoriales",
+    "info":           "Mostrando _START_ a _END_ de _TOTAL_ editoriales",
+    "infoEmpty":      "Mostrando 0 a 0 de 0 editoriales",
+    "paginate": {
+      "first":      "Primera",
+      "last":       "Última",
+      "next":       "»",
+      "previous":   "«"
+    },
+  },
+  [/wp-datatable]');
+}
+
+$index = 0;
 ?>
 
 <section>
-  <table id="table" class="display compact" style="width:100%">
+    <table id="table" class="display compact" style="width:100%">
 
-    <thead>
-    <tr>
-      <th>#</th>
-      <th> <?php echo $taxonomyName; ?> </th>
-      <th>PUBLICACIONES</th>
-    </tr>
-    </thead>
-
-    <tbody>
-    <?php
-      foreach ($taxonomyTerms as $taxTerm) {
-        $index++;
-        ?>
+        <thead>
         <tr>
-          <td><?php echo $index ?></td>
-          <td>
-            <?php echo '<a href="' . esc_attr (get_term_link ($taxTerm, $taxonomy)) . '" title="' . sprintf (__ ("Ver todos las entradas de %s"), $taxTerm->name) . '" ' . '>' . $taxTerm->name . '</a>'; ?>
-          </td>
-          <td><?php $taxonomy = $taxTerm->taxonomy;
-              getTotalsPosts ($taxTerm, $taxonomy); ?></td>
-          </td>
+            <th>#</th>
+            <th> <?php echo $taxonomyName; ?> </th>
+            <th>LIBROS</th>
         </tr>
-      <?php } ?>
-    </tbody>
+        </thead>
 
-    <tfoot>
-    <tr>
-      <th>#</th>
-      <th> <?php echo $taxonomyName; ?> </th>
-      <th>PUBLICACIONES</th>
-    </tr>
-    </tfoot>
+        <tbody>
+        <?php
+        foreach ($taxonomyTerms as $taxTerm) {
+            $index++;
+            ?>
+            <tr>
+                <td><?php echo $index ?></td>
+                <td>
+                    <?php echo '<a href="' . esc_attr(get_term_link($taxTerm, $taxonomy)) . '" title="' . sprintf(__("Ver todos las entradas de %s"), $taxTerm->name) . '" ' . '>' . $taxTerm->name . '</a>'; ?>
+                </td>
+                <td><?php
+                    $taxonomy = $taxTerm->taxonomy;
+                    $term = $taxTerm->slug;
+                    $total = getTotalsPosts($taxonomy, $term);
+                    echo $total;
+                    ?></td>
+                </td>
+            </tr>
+        <?php } ?>
+        </tbody>
 
-  </table>
+        <tfoot>
+        <tr>
+            <th>#</th>
+            <th> <?php echo $taxonomyName; ?> </th>
+            <th>LIBROS</th>
+        </tr>
+        </tfoot>
+
+    </table>
 </section>
